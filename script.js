be52e630c2f19c829e7354c12c98cd44ee68cbca
Erfik8 <jakubkubica1@gmail.com>
2022-07-21 19:10:59 +0200

    added next carousel, code refactor, changed background, added interface to easily change hexbox size

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let mediacarousel = document.getElementById("MediaPartners");
+let eventcarousel = document.getElementById("EventPartners");
 
-items.forEach((el) => {
+let mediaitems = mediacarousel.querySelectorAll('.carousel .carousel-item');
+let eventitems = eventcarousel.querySelectorAll('.carousel .carousel-item');
+
+mediaitems.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
+        	next = mediaitems[0]
       	}
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
     }
-})
+});
+eventitems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = eventitems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+});
