4ed136067c4376b251d4be2e4f6fc72d6a11603e
Erfik8 <jakubkubica1@gmail.com>
2022-08-11 16:09:31 +0200

    soo many changes. Changed colors, menu behavior, added new slider (not finished), added FAQ section, changed sponsors section

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ let eventcarousel = document.getElementById("EventPartners");
 let mediaitems = mediacarousel.querySelectorAll('.carousel .carousel-item');
 let eventitems = eventcarousel.querySelectorAll('.carousel .carousel-item');
 
+let FqaElements = document.querySelectorAll('.FqaQuestionConteiner');
+var checkedElement = null;
+
+FqaElements.forEach((el) => {
+    el.getElementsByTagName('h4')[0].addEventListener('click',function(){
+        if(checkedElement != null) checkedElement.id = "";
+        this.id = "checked";
+        checkedElement = this;
+        console.log(this);
+    })
+});
+
 mediaitems.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
